1782990257ece94bc91f46f95bb66adb803bf65a
Doug Alderman <agnadcp-d1@yahoo.com>
2022-01-05 10:47:36 -0500

    Removed Amplify files

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "blogfinal": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
-
-Amplify.configure(aws_exports);
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
