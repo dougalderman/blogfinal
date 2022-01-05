4bcfed7c400df8b2305ae8b7131e345d1671b877
Doug Alderman <agnadcp-d1@yahoo.com>
2022-01-05 10:00:27 -0500

    Added Amplify to app. Created a new DisplayPosts module.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import DisplayPosts from './components/DisplayPosts';
 
 function App() {
   return (
     <div className="App">
-     
+     < DisplayPosts />
     </div>
   );
 }
