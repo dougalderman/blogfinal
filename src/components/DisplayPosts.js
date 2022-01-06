04eb0b433893b3adbb0e1e7e1508870650910726
Doug Alderman <agnadcp-d1@yahoo.com>
2022-01-06 14:14:50 -0500

    Queried posts and displayed them on page.

--- src/components/DisplayPosts.js
+++ src/components/DisplayPosts.js
@@ -4,19 +4,54 @@ import { API, graphqlOperation } from 'aws-amplify';
 
 class DisplayPosts extends Component {
 
+  state = {
+    posts: []
+  }
+  
   componentDidMount = async () => {
     this.getPosts();
   }
 
   getPosts = async () => {
     const result = await API.graphql(graphqlOperation(listPosts));
-    console.log('All Posts: ', result.data.listPosts.items);
+    
+    this.setState({ posts: result.data.listPosts.items})
+    
+    // console.log('All Posts: ', result.data.listPosts.items);
   }
   
   render() {
-    return (
-      <div>Hello World</div>
-    );
+    const { posts }  = this.state;
+    return posts.map(( post ) => {
+
+      return (
+        <div className="posts" style={rowStyle} key={ post.id }>
+          <h1> { post.postTitle }</h1>
+          <span style={{ fontStyle: "italic", color: "#0ca5e297" }}>
+            { "Wrote by: " } { post.postOwnerUsername }
+
+            { " on  "}
+            <time style={{ fontStyle: "italic" }}>
+              { " " }
+              { new Date(post.createdAt).toDateString() }
+            </time>
+          </span>
+
+          <p>
+            { post.postBody }
+          </p>
+
+        </div>
+      )
+
+    });
   }
+}
+
+const rowStyle = {
+  background: '#f4f4f4',
+  padding: '10px',
+  border: '1px #ccc dotted',
+  margin: '14px'
 }
 export default DisplayPosts;
