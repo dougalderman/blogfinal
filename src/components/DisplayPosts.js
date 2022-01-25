8ec5be42206a2a15db517c7c174e33094bdebc03
Doug Alderman <agnadcp-d1@yahoo.com>
2022-01-25 11:18:44 -0500

    Added functionality for deleting posts.

--- src/components/DisplayPosts.js
+++ src/components/DisplayPosts.js
@@ -3,7 +3,7 @@ import { listPosts } from '../graphql/queries';
 import { API, graphqlOperation } from 'aws-amplify';
 import DeletePost  from './DeletePosts';
 import EditPost from './EditPost';
-import { onCreatePost } from '../graphql/subscriptions';
+import { onCreatePost, onDeletePost } from '../graphql/subscriptions';
 
 class DisplayPosts extends Component {
 
@@ -24,10 +24,22 @@ class DisplayPosts extends Component {
           this.setState({ posts: updatedPosts});
         }
       });
+
+      this.deletePostListener = API.graphql(graphqlOperation(onDeletePost))
+        .subscribe({
+          next: postData => {
+
+            const deletedPost = postData.value.data.onDeletePost;
+            const updatedPosts = this.state.posts.filter(post => post.id !== deletedPost.id);
+            this.setState({posts: updatedPosts});
+
+          }
+        })
   }
 
   componentWillUnmount() {
     this.createPostListener.unsubscribe();
+    this.deletePostListener.unsubscribe();
   }
 
   getPosts = async () => {
@@ -63,7 +75,7 @@ class DisplayPosts extends Component {
           <br />
 
           <span>
-            <DeletePost />
+            <DeletePost data={post}/>
             <EditPost />
           </span>
 
